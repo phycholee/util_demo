40a5d021ef23d14569aa4a4e9e5f7d9656d155ab
phycholee <962109025@qq.com>
2018-04-19 00:09:49 +0800

    use jose4j jwt

--- src/main/java/com/llf/demo/util/JwtUtil.java
+++ src/main/java/com/llf/demo/util/JwtUtil.java
@@ -1,17 +1,24 @@
 package com.llf.demo.util;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.Verification;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.lang.JoseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import sun.misc.BASE64Decoder;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
 
 /**
  * @author: Oliver.li
@@ -22,35 +29,90 @@ public class JwtUtil {
 
     private static Logger logger = LoggerFactory.getLogger(JwtUtil.class);
 
-    public static String createToken(long exp) throws UnsupportedEncodingException {
+    private static Key privateKey;
+    private static Key publicKey;
 
-        Algorithm algorithm = Algorithm.HMAC256("secret");
-
-        long expireMillis = System.currentTimeMillis() + exp;
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(expireMillis);
-        Date expireTime = calendar.getTime();
-
-        String token = JWT.create()
-                .withClaim("userId", "1")
-                .withClaim("role", "ADMIN")
-                .withExpiresAt(expireTime).sign(algorithm);
-
-        return token;
+    private JwtUtil() {
     }
 
-    public static String checkToken(String token) throws UnsupportedEncodingException {
+    public static String generateToken(String issUser, String audience, Float minutes, Map<String, Object> payLoadMap) throws JoseException {
+        if(StringUtils.isEmpty(issUser)) {
+            logger.debug("令牌创建者为空");
+            return null;
+        } else if(StringUtils.isEmpty(audience)) {
+            logger.debug("令牌使用者为空");
+            return null;
+        } else if(minutes == null) {
+            logger.debug("令牌有效时间为空");
+            return null;
+        } else {
+            JwtClaims claims = new JwtClaims();
+            claims.setIssuer(issUser);
+            claims.setAudience(issUser);
+            claims.setExpirationTimeMinutesInTheFuture(minutes.floatValue());
+            claims.setGeneratedJwtId();
+            claims.setIssuedAtToNow();
+            claims.setNotBeforeMinutesInThePast(1.0F);
+            claims.setSubject("subject");
+            Iterator var5 = payLoadMap.entrySet().iterator();
 
-        return null;
+            while(var5.hasNext()) {
+                Map.Entry<String, Object> entry = (Map.Entry)var5.next();
+                String key = ((String)entry.getKey()).toString();
+                String value = entry.getValue() == null?"":entry.getValue().toString();
+                claims.setClaim(key, value);
+            }
+
+            claims.setClaim("audience", audience);
+            JsonWebSignature jws = new JsonWebSignature();
+            jws.setPayload(claims.toJson());
+            jws.setKey(privateKey);
+            jws.setKeyIdHeaderValue("k1");
+            jws.setAlgorithmHeaderValue("RS256");
+            return jws.getCompactSerialization();
+        }
     }
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String token = createToken(60 * 10);
-        logger.info(token);
+    public static Map<String, Object> checkToken(String token, String isUser, String audience) throws InvalidJwtException {
+        JwtConsumer jwtConsumer = (new JwtConsumerBuilder()).setRequireExpirationTime().setAllowedClockSkewInSeconds(30).setRequireSubject().setExpectedIssuer(isUser).setExpectedAudience(new String[]{audience}).setVerificationKey(publicKey).build();
+        JwtClaims jwtClaims = jwtConsumer.processToClaims(token);
+        Map<String, Object> payLoadMap = jwtClaims.getClaimsMap();
+        logger.debug("token 校验成功" + payLoadMap);
+        return payLoadMap;
+    }
 
-        String s = checkToken(token);
-        logger.info(s);
+    static {
+        try {
+            Configuration configuration = (new Configurations()).properties(new File("/", "jwt.properties"));
+            byte[] keyBytes = (new BASE64Decoder()).decodeBuffer(configuration.getString("privateKey"));
+            PKCS8EncodedKeySpec keySpec_privateKey = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory_privateKey = KeyFactory.getInstance("RSA");
+            privateKey = keyFactory_privateKey.generatePrivate(keySpec_privateKey);
+            keyBytes = (new BASE64Decoder()).decodeBuffer(configuration.getString("publicKey"));
+            X509EncodedKeySpec keySpec_publicKey = new X509EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory_publicKey = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory_publicKey.generatePublic(keySpec_publicKey);
+        } catch (Exception var6) {
+            logger.error("", var6);
+            System.exit(1);
+        }
+
+    }
+
+    public static void main(String[] args) throws InvalidJwtException, JoseException {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("aa", "AA");
+
+        String token = generateToken("test", "test", 2.0f, map);
+
+        System.out.println("token:" + token);
+
+        Map<String, Object> checkToken = checkToken(token, "test", "test");
+
+        System.out.println("checkToken:" + checkToken);
+
+
     }
 
 }
--- src/test/java/com/llf/demo/controller/HateControllerTest.java
+++ src/test/java/com/llf/demo/controller/HateControllerTest.java
@@ -1,9 +1,19 @@
 package com.llf.demo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.llf.demo.DemoApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.text.SimpleDateFormat;
 
 import static org.junit.Assert.*;
 
@@ -13,9 +23,17 @@ import static org.junit.Assert.*;
  * @date: 2018/4/18 11:08
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HateControllerTest {
 
+    @Autowired
+    private WebApplicationContext webApp;
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() throws Exception{
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApp).build();
+    }
 
     @Test
     public void get() {
